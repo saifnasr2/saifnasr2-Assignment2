0f064c71b1411a6157723a70bb5966b89dde1b3c
Saif Nasr <saifnasr418@gmail.com>
2026-09-13 11:34:42 +0300

    initialize calculator

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def inputs(num1 , num2 , operator):
+    pass
+
+
+while True:
+    print("""=========Choose an Option===========
+    1-Make a Calculation
+    2-Exit\n\n""")
+    choice = input("What is your choice:")
+
+    if choice == "1":
+        num1 = int(input("Enter the first number:"))
+        num2 = int(input("Enter the second number:"))
+        operation = input("Enter your operation:")
+    else:   
+        break
+  
+
+
+
+    
+
+
+
